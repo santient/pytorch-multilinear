042c87fb56a4928d6a4fd2d882f1ed763e16cfea
Santiago Benoit <santib27@gmail.com>
2020-10-04 00:56:20 -0400

    add support for batched vectors using einsum

--- multilinear.py
+++ multilinear.py
@@ -1,25 +1,24 @@
 import math
+import string
 
 import torch
 from torch import nn
 from torch.nn import init
 
 
-def multilinear(inputs, weight, bias=None):
-    assert weight.dim() == len(inputs) + 1
-    out = weight
-    for input in reversed(inputs):
-        out = out @ input
-    if bias is not None:
-        out = out + bias
-    return out
-
 class Multilinear(nn.Module):
     def __init__(self, in_features, out_features, bias=True):
         super().__init__()
         self.in_features = tuple(in_features)
+        if len(self.in_features) > 24:
+            raise ValueError('Up to 24 input vectors supported')
         self.out_features = out_features
         self.weight = nn.Parameter(torch.Tensor(out_features, *in_features))
+        chars = string.ascii_lowercase
+        n = len(self.in_features)
+        self.einsum_str = '{}{},z{}->z{}'.format(
+            chars[n], chars[:n], ',z'.join(chars[:n]), chars[n]
+        )
         if bias:
             self.bias = nn.Parameter(torch.Tensor(out_features))
         else:
@@ -33,7 +32,10 @@ class Multilinear(nn.Module):
             init.uniform_(self.bias, -bound, bound)
 
     def forward(self, *inputs):
-        return multilinear(inputs, self.weight, self.bias)
+        out = torch.einsum(self.einsum_str, self.weight, *inputs)
+        if self.bias is not None:
+            out = out + self.bias
+        return out
 
     def extra_repr(self):
         return 'in_features={}, out_features={}, bias={}'.format(
